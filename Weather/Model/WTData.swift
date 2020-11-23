31d23a6de56cf04cf0359e808669d41dc90f3e1c
varadzin <varadzin@gmail.com>
2020-11-23 21:00:21 +0100

    second version

--- Weather/Model/WTData.swift
+++ Weather/Model/WTData.swift
@@ -7,215 +7,27 @@
 
 import Foundation
 
-struct HolidayResponse: Decodable {
-    var list: [Holidays]
-}
-
-
-
-struct Holidays: Decodable {
-    var main: HolidayDetail
-    var weather: HolidayDetail2
-    var wind: HolidayDetail3
-}
-
-
-struct HolidayDetail: Decodable {
-    var temp: Double
-    var feels_like: Double
+struct WTData: Codable {
     
+    let name: String
+    let main: Main
+    let weather: [Weather]
+    let wind: Wind
 }
 
-struct HolidayDetail2 : Decodable {
-    var description: String
-    var icon: String
+struct Main: Codable {
+    let temp: Double
 }
 
-struct HolidayDetail3 : Decodable {
-    var speed: Double
+struct Weather: Codable {
+    let description: String
+    let icon: String
+    let main: String
 }
 
-struct WeatherCity: Decodable {
-    var city: CityDetail
-}
-
-struct CityDetail: Decodable {
-    var name: String
+struct Wind: Codable {
+    let speed: Double
 }
 
 
 
-
-//struct cityDetail: Decodable {
-//    var city: cityName
-//}
-//
-//struct lists: Decodable {
-//    var list:[listDetail]
-//}
-//
-//struct listDetail: Decodable {
-//    var main: mainDetail
-//    var weather: weatherDetail
-//    var wind: windDetail
-//}
-//
-//struct mainDetail: Decodable {
-//    var temp: Double
-//}
-//
-//struct weatherDetail: Decodable {
-//    var description: String
-//    var icon: String
-//}
-//
-//struct windDetail: Decodable {
-//    var speed: Double
-//}
-//
-//struct cityName: Decodable {
-//    var name: String
-//}
-
-
-/*
- FORECAST
- 
- {
-     "cod": "200",
-     "message": 0,
-     "cnt": 40,
-     "list": [
-         {
-             "dt": 1605906000,
-             "main": {
-                 "temp": 10.27,
-                 "feels_like": 6.5,
-                 "temp_min": 10.24,
-                 "temp_max": 10.27,
-                 "pressure": 1030,
-                 "sea_level": 1030,
-                 "grnd_level": 1027,
-                 "humidity": 88,
-                 "temp_kf": 0.03
-             },
-             "weather": [
-                 {
-                     "id": 804,
-                     "main": "Clouds",
-                     "description": "overcast clouds",
-                     "icon": "04n"
-                 }
-             ],
-             "clouds": {
-                 "all": 95
-             },
-             "wind": {
-                 "speed": 4.85,
-                 "deg": 237
-             },
-             "visibility": 10000,
-             "pop": 0,
-             "sys": {
-                 "pod": "n"
-             },
-             "dt_txt": "2020-11-20 21:00:00"
-         },
- 
- 
- ],
-     "city": {
-         "id": 2643743,
-         "name": "London",
-         "coord": {
-             "lat": 51.5085,
-             "lon": -0.1257
-         },
-         "country": "GB",
-         "population": 1000000,
-         "timezone": 0,
-         "sunrise": 1605857261,
-         "sunset": 1605888305
-     }
- }
- 
- WEATHER
- 
- {
-     "coord": {
-         "lon": -0.13,
-         "lat": 51.51
-     },
-     "weather": [
-         {
-             "id": 804,
-             "main": "Clouds",
-             "description": "overcast clouds",
-             "icon": "04n"
-         }
-     ],
-     "base": "stations",
-     "main": {
-         "temp": 10.3,
-         "feels_like": 7.02,
-         "temp_min": 10,
-         "temp_max": 11,
-         "pressure": 1030,
-         "humidity": 87
-     },
-     "visibility": 10000,
-     "wind": {
-         "speed": 4.1,
-         "deg": 230
-     },
-     "clouds": {
-         "all": 90
-     },
-     "dt": 1605903167,
-     "sys": {
-         "type": 1,
-         "id": 1414,
-         "country": "GB",
-         "sunrise": 1605857262,
-         "sunset": 1605888305
-     },
-     "timezone": 0,
-     "id": 2643743,
-     "name": "London",
-     "cod": 200
- }
- 
- 
- 
- struct WTData: Codable {
-     
-     let list: [List]
- }
-     
- struct List: Codable {
-     
-     let name: String
-     let main: Main
-     let weather: [Weather]
-     let wind: Wind
- }
-
- struct Main: Codable {
-     let temp: Double
- }
-
- struct Weather: Codable {
-     let description: String
-     let icon: String
-     let main: String
- }
-
- struct Wind: Codable {
-     let speed: Double
- }
-
-
- 
- 
- 
- */
--- Weather/Model/WTManager.swift
+++ Weather/Model/WTManager.swift
@@ -13,57 +13,45 @@ protocol WTManagerDelegate {
 }
 
 struct WTManager {
-    var resourseURL: URL
     
-//
-//    let weatherURL = "https://api.openweathermap.org/data/2.5/forecast?appid=1513074b04afcac9adc59f2ce25f6755&units=metric"
-//
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=1513074b04afcac9adc59f2ce25f6755&units=metric"
+    
     // forecast: https://api.openweathermap.org/data/2.5/forecast?appid=1513074b04afcac9adc59f2ce25f6755&units=metric&q=london
     var delegate: WTManagerDelegate?
     
     func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(with: urlString)
+    }
     
-       
-        let resourceString = "https://api.openweathermap.org/data/2.5/forecast?appid=1513074b04afcac9adc59f2ce25f6755&units=metric&q=\(cityName)"
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session =  URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                if let safeData = data {
+                    if let weather = self.parseJSON(safeData) {
+                        self.delegate?.didUpdateWeather(self, weather: weather)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ weatherData: Data) -> WTModel? {
+        let decoder = JSONDecoder()
         
-        guard let resourceURL = URL(string: resourceString) else { fatalError()}
-        
-
-       
-       func performRequest( {
-        
-        
-        
-        
-        
-//           if let url = URL(string: urlString) {
-//               let session =  URLSession(configuration: .default)
-//               let task = session.dataTask(with: url) { (data, response, error) in
-//                   if error != nil {
-//                       self.delegate?.didFailWithError(error: error!)
-//                       return
-//                   }
-//                   if let safeData = data {
-//                       if let weather = self.parseJSON(safeData) {
-//                           self.delegate?.didUpdateWeather(self, weather: weather)
-//                       }
-//                   }
-//               }
-//               task.resume()
-//           }
-//       }
-//
-       func parseJSON(_ weatherData: Data) -> WTModel? {
-           let decoder = JSONDecoder()
-           
-           do {
-               let decodedData = try decoder.decode(lists.self, from: weatherData)
-        
-            let temp = decodedData.list.main.temp
-            let name = decodedData.list.name
-            let description = decodedData.list.weather.description
-            let speed = decodedData.list.wind.speed
-            let icon = decodedData.list.weather.icon
+        do {
+            let decodedData = try decoder.decode(WTData.self, from: weatherData)
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            let description = decodedData.weather[0].description
+            let speed = decodedData.wind.speed
+            let icon = decodedData.weather[0].icon
             
             let weather = WTModel(weatherDescription: description, cityName: name, temperature: temp, windSpeed: speed, weatherIcon: icon)
             
