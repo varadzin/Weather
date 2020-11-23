0878f726a57ce2e18f93129f4d7e676863ea0ec8
varadzin <varadzin@gmail.com>
2020-11-23 21:54:19 +0100

    FC NetworkManaager

---
+++ Weather/Model/FCManager.swift
@@ -0,0 +1,16 @@
+//
+//  FCManager.swift
+//  Weather
+//
+//  Created by Frantisek Varadzin on 23/11/2020.
+//
+
+import Foundation
+
+enum DataError: Error {
+    case noDataAvailable
+    case canNotProcessData
+    
+}
+
+
